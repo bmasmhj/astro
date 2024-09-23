186efe9372ef4dc8d9e52c78d77253a9cb6c967b
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2024-09-23 13:07:44 +0545

    Refactor Astro config to handle Storyblok API key and local environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,18 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import mkcert from 'vite-plugin-mkcert';
 import tailwind from '@astrojs/tailwind';
-import { loadEnv } from 'vite';
 import storyblok from '@storyblok/astro';
-import { isPreview } from './src/utils/isPreview';
 import vercel from "@astrojs/vercel/serverless";
+import { loadEnv } from 'vite';
+import { isPreview } from './src/utils/isPreview';
 
 const { STORYBLOK_API_KEY, STORYBLOK_LOCAL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-
 const isLocal = STORYBLOK_LOCAL === "true";
 
 export default defineConfig({
   integrations: [
     vue(),
     tailwind(),
-    // Storyblok integration
     storyblok({
       accessToken: STORYBLOK_API_KEY,
       livePreview: isPreview,
@@ -32,23 +30,30 @@ export default defineConfig({
 
   output: "server",
 
-  // Vite Configuration for Local HTTPS
   ...(isLocal && {
     vite: {
       server: {
         https: true,
       },
       plugins: [mkcert()],
-    }
+      build: {
+        rollupOptions: {
+          onwarn(warning, warn) {
+            // Silence certain warnings (example: "THIS_IS_UNDEFINED")
+            if (warning.code === 'THIS_IS_UNDEFINED') return;
+            warn(warning); // let others pass through
+          },
+        },
+      }
+    },
   }),
 
-  // Vercel Adapter Configuration
   adapter: vercel(
     isPreview
-      ? {} // Preview settings
+      ? {}
       : {
           isr: {
-            expiration: 60, // Incremental Static Regeneration every 60 seconds
+            expiration: 60,
           },
         }
   )
