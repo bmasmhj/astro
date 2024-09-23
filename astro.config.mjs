434147abc76b810d5e00df57791a65035822567c
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2024-09-23 13:26:20 +0545

    revert

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,43 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-// import mkcert from 'vite-plugin-mkcert';
+import mkcert from 'vite-plugin-mkcert';
 import tailwind from '@astrojs/tailwind';
-import storyblok from '@storyblok/astro';
-import vercelServerless from '@astrojs/vercel/serverless';
 import { loadEnv } from 'vite';
+import storyblok from '@storyblok/astro';
 import { isPreview } from './src/utils/isPreview';
-
-const { STORYBLOK_API_KEY } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-// const isLocal = STORYBLOK_LOCAL === "true";
-
+import vercel from "@astrojs/vercel/serverless";
+const {STORYBLOK_API_KEY , STORYBLOK_LOCAL } = loadEnv(process.env.NODE_ENV, process.cwd() , "");
+const isLocal = STORYBLOK_LOCAL === "true";
 export default defineConfig({
-  integrations: [
-    vue(),
-    tailwind(),
+  integrations: [vue(), tailwind(), 
+    //  add storyblok integration
     storyblok({
-      accessToken: STORYBLOK_API_KEY,
-      livePreview: isPreview,
-      enableFallbackComponent: true,
-      components: {
-        default_page: "storyblok/DefaultPage",
-        hero_section: "storyblok/HeroSection",
-        grid: "storyblok/FeaturedSection",
-        newsletter: "storyblok/NewsletterSection",
-        technology: "storyblok/TechnologySection",
-      }
+        accessToken: STORYBLOK_API_KEY,
+        livePreview : isPreview,
+        enableFallbackComponent : true,
+        components : {
+          default_page : "storyblok/DefaultPage",
+          hero_section : "storyblok/HeroSection",
+          grid : "storyblok/FeaturedSection",
+          newsletter : "storyblok/NewsletterSection",
+          technology : "storyblok/TechnologySection",
+
+        }
     }),
   ],
 
-  output: "server",
-  adapter: vercelServerless()
+  output : "server",
+
+  ...(isLocal && { vite : {
+    server : {
+        https : true
+    },
+    plugins : [mkcert()]
+  }}),
+
+  adapter: vercel(isPreview ? {} :{
+    isr : {
+      expiration : 60,
+    }
+  })
 });
