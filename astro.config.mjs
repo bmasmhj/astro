478e81cec7aac70021ea096d56d6339107e50ea8
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2024-09-23 13:17:23 +0545

    astro without vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,14 @@ import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 import storyblok from '@storyblok/astro';
 import vercelServerless from '@astrojs/vercel/serverless';
-import { loadEnv } from 'vite';
 import { isPreview } from './src/utils/isPreview';
-
-const { STORYBLOK_API_KEY } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-// const isLocal = STORYBLOK_LOCAL === "true";
 
 export default defineConfig({
   integrations: [
     vue(),
     tailwind(),
     storyblok({
-      accessToken: STORYBLOK_API_KEY,
+      accessToken: import.meta.env.STORYBLOK_API_KEY,
       livePreview: isPreview,
       enableFallbackComponent: true,
       components: {
