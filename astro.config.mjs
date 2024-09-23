25b78a5ea68eb55c635d3dab8ebd72569942519b
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2024-09-23 13:21:05 +0545

    Refactor Astro config to remove vite dependency and unused variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+// import mkcert from 'vite-plugin-mkcert';
 import tailwind from '@astrojs/tailwind';
 import storyblok from '@storyblok/astro';
 import vercelServerless from '@astrojs/vercel/serverless';
+import { loadEnv } from 'vite';
 import { isPreview } from './src/utils/isPreview';
+
+const { STORYBLOK_API_KEY } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+// const isLocal = STORYBLOK_LOCAL === "true";
 
 export default defineConfig({
   integrations: [
     vue(),
     tailwind(),
     storyblok({
-      accessToken: import.meta.env.STORYBLOK_API_KEY,
+      accessToken: STORYBLOK_API_KEY,
       livePreview: isPreview,
       enableFallbackComponent: true,
       components: {
