2b3277192a3bc9232702d0cca28b74ea8ada944d
Bimash Maharjan <87689921+bmasmhj@users.noreply.github.com>
2024-09-23 13:02:34 +0545

    Refactor Astro config to handle Storyblok API key and local environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,38 +6,50 @@ import { loadEnv } from 'vite';
 import storyblok from '@storyblok/astro';
 import { isPreview } from './src/utils/isPreview';
 import vercel from "@astrojs/vercel/serverless";
-const {STORYBLOK_API_KEY , STORYBLOK_LOCAL } = loadEnv(process.env.NODE_ENV, process.cwd() , "");
-const isLocal = STORYBLOK_LOCAL === "true";
-export default defineConfig({
-  integrations: [vue(), tailwind(), 
-    //  add storyblok integration
-    storyblok({
-        accessToken: STORYBLOK_API_KEY,
-        livePreview : isPreview,
-        enableFallbackComponent : true,
-        components : {
-          default_page : "storyblok/DefaultPage",
-          hero_section : "storyblok/HeroSection",
-          grid : "storyblok/FeaturedSection",
-          newsletter : "storyblok/NewsletterSection",
-          technology : "storyblok/TechnologySection",
 
-        }
+const { STORYBLOK_API_KEY, STORYBLOK_LOCAL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
+const isLocal = STORYBLOK_LOCAL === "true";
+
+export default defineConfig({
+  integrations: [
+    vue(),
+    tailwind(),
+    // Storyblok integration
+    storyblok({
+      accessToken: STORYBLOK_API_KEY,
+      livePreview: isPreview,
+      enableFallbackComponent: true,
+      components: {
+        default_page: "storyblok/DefaultPage",
+        hero_section: "storyblok/HeroSection",
+        grid: "storyblok/FeaturedSection",
+        newsletter: "storyblok/NewsletterSection",
+        technology: "storyblok/TechnologySection",
+      }
     }),
   ],
 
-  output : "server",
+  output: "server",
 
-  ...(isLocal && { vite : {
-    server : {
-        https : true
-    },
-    plugins : [mkcert()]
-  }}),
-
-  adapter: vercel(isPreview ? {} :{
-    isr : {
-      expiration : 60,
+  // Vite Configuration for Local HTTPS
+  ...(isLocal && {
+    vite: {
+      server: {
+        https: true,
+      },
+      plugins: [mkcert()],
     }
-  })
+  }),
+
+  // Vercel Adapter Configuration
+  adapter: vercel(
+    isPreview
+      ? {} // Preview settings
+      : {
+          isr: {
+            expiration: 60, // Incremental Static Regeneration every 60 seconds
+          },
+        }
+  )
 });
